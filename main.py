ff4d3d39d746df2ff664fb1ec7854c815a8a4e7f
SheljaRajan <sheljarajan@gmail.com>
2022-07-01 11:00:46 +0200

    Initial working commit which plots modulo 10 of randomly generated values.

---
+++ main.py
@@ -0,0 +1,23 @@
+# Demo application for JCB
+
+import numpy as np
+import pandas as pd
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+generatedDataFrame = {}
+@app.route("/", methods=['GET'])
+def home():
+    return render_template("home.html", x=generatedDataFrame)
+
+@app.route("/data", methods=['GET'])
+def get_df():
+    data = np.random.randint(0, 100, size=1000)
+    df = pd.DataFrame(data, columns=['a'])
+    df['b'] = df.mod(10)
+    generatedDataFrame = df.to_json(orient='records')
+    return generatedDataFrame
+
+if __name__ == '__main__':
+    app.run(debug=True)
